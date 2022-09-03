9fd62b637c9bdfefbce01eb16e4228e65e91a436
YuriyGubin <iosdevelop.2022@gmail.com>
2022-09-03 13:44:39 +0500

    Initial Commit

---
+++ TotalBill/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TotalBill
+//
+//  Created by Yuriy on 03.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
