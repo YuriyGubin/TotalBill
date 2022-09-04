e8960d4ae6f3383eeca8deb8cb695978bf2b97a4
YuriyGubin <iosdevelop.2022@gmail.com>
2022-09-04 16:38:01 +0500

    Add logo (racoon)

--- TotalBill/MainViewController.swift
+++ TotalBill/MainViewController.swift
@@ -17,6 +17,13 @@ class MainViewController: UIViewController {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
+    
+    let logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "enot")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,7 +35,9 @@ class MainViewController: UIViewController {
     
     func setupViews() {
         view.backgroundColor = #colorLiteral(red: 0.9764705882, green: 0.9764705882, blue: 0.9764705882, alpha: 1)
+        
         view.addSubview(titleLabel)
+        view.addSubview(logoImageView)
     }
     
     
@@ -42,7 +51,11 @@ extension MainViewController {
         NSLayoutConstraint.activate([
             titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
             titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-        
+            
+            logoImageView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
+            logoImageView.heightAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
         
         ])
     }
